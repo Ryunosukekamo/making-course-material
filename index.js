53b481ecf774a5efe2ef63494caf8ee11c932a5c
加茂竜之介 <Kamoryu2001@gmail.com>
2022-07-01 02:41:10 +0900

    基本機能の完成with 無駄なところを消去

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const chaningMainContent = () => {
         // inputに入力した値を取得
         let mainContentFiled = document.getElementById('main_content_filed');
         let mainContentFiledValue = mainContentFiled.value;
-        console.log(mainContentFiledValue);
 
         //リボンの長さを変更
         let mainContent = document.getElementById('main_contet');
@@ -30,7 +29,6 @@ const chaningSecondContent = () => {
     // もう一方に入力した値を取得
     let displayingContentFiled = document.getElementById('displaying_content_filed');
     let displayingContentFiledValue = displayingContentFiled.value;
-    console.log(displayingContentFiledValue);
 
     // リボンの長さを変更して、表示
     let displayingContent = document.getElementById('displaying_content');
