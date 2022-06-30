d12f430f6dbab3dc59a2d0a4395dd66edd19dfa5
加茂竜之介 <Kamoryu2001@gmail.com>
2022-07-01 01:04:42 +0900

    mainContent変更のための長さを取得可能に

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const chaningMainContent = () => {
+    let mainContent = document.getElementById('main_content');
+    let mainContentValue = mainContent.value;
+    console.log(mainContentValue);
+}
