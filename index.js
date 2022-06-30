d87b62b2ac2796fd14284706419ed9e0fbc88057
加茂竜之介 <Kamoryu2001@gmail.com>
2022-07-01 02:39:47 +0900

    基本機能の完成

--- index.js
+++ index.js
@@ -2,34 +2,39 @@
 
 const chaningMainContent = () => {
 
-    // inputに入力した値を取得
-    let mainContentFiled = document.getElementById('main_content_filed');
-    let mainContentFiledValue = mainContentFiled.value;
-    console.log(mainContentFiledValue);
-
-    //リボンの長さを変更
-    let mainContent = document.getElementById('main_contet');
-    let valueForWith = `${mainContentFiledValue}%`;
-    mainContent.style.width = `${valueForWith}`;
-
-    chaningSecondContent(valueForWith);
-}
-
-const chaningSecondContent = (valueForWith) => {
-    if (valueForWith) {
-        // もう一方の入力欄を表示
-        let displayingContentFiled = document.getElementById('displaying_content_filed');
-        displayingContentFiled.style.display = "block";
-
-        // もう一方に入力した値を取得
-        let displayingContentFiledValue = displayingContentFiled.value;
-        console.log(displayingContentFiledValue);
+    new Promise(resolve => {
+        // inputに入力した値を取得
+        let mainContentFiled = document.getElementById('main_content_filed');
+        let mainContentFiledValue = mainContentFiled.value;
+        console.log(mainContentFiledValue);
 
         //リボンの長さを変更
-        // let mainContent = document.getElementById('main_contet');
-        // valueForWith = `${mainContentFiledValue}%`;
-        // mainContent.style.width = `${valueForWith}`;
-    } else {
-        return;
-    }
+        let mainContent = document.getElementById('main_contet');
+        mainContent.style.width = `${mainContentFiledValue}%`;
+        resolve();
+
+    }).then(() => {
+        return new Promise(resolve => {
+            // もう一方の入力欄を表示
+            let displayingContentFiled = document.getElementById('displaying_content_filed');
+            displayingContentFiled.style.display = "block";
+            resolve();
+
+        })
+    })
+
+
+}
+
+const chaningSecondContent = () => {
+    // もう一方に入力した値を取得
+    let displayingContentFiled = document.getElementById('displaying_content_filed');
+    let displayingContentFiledValue = displayingContentFiled.value;
+    console.log(displayingContentFiledValue);
+
+    // リボンの長さを変更して、表示
+    let displayingContent = document.getElementById('displaying_content');
+    displayingContent.style.width = `${displayingContentFiledValue}%`;
+    displayingContent.style.display = "block";
+
 }
